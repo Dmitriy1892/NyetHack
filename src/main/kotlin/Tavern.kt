013513e816440e689d5d553eb6c7265e42d6cf2d
Dmitriy1892 <kodim1892@gmail.com>
2021-01-11 17:58:48 +0300

    page 234

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -3,13 +3,15 @@ import kotlin.math.roundToInt
 
 const val TAVERN_NAME = "Taernyl's Folly"
 
-var playerGold = 10
-var playerSilver = 10
-
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
 val menuList = File("data/tavern-menu-items.txt").readText().split("\n")
+val patronGolg = mutableMapOf<String, Double>()
+
 
 fun main(args: Array<String>) {
+//    menuPrinted(menuList)
     if (patronList.contains("Eli")) {
         println("The tavern master says: Eli's in the back playing cards.")
     } else {
@@ -22,34 +24,65 @@ fun main(args: Array<String>) {
         println("The tavern master says: Nay, they departed hours ago.")
     }
 
-    patronList.forEachIndexed { index, patron ->
-        println("Good evening, $patron - you're #${index + 1} in line")
-        placeOrder(patron, menuList.shuffled().first().replace("\r", ""))
+    (0..9).forEach {
+        val first  =patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
     }
 
-    menuList.forEachIndexed { index, data ->
-        println("$index : $data")
+    uniquePatrons.forEach {
+        patronGolg[it] = 6.0
+    }
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first().replace("\r", ""))
+        orderCount++
+    }
+
+    displayPatronBalances()
+}
+
+//fun menuPrinted(menuList: List<String>) {
+//    println("*** Welcome to Taernyl's Folly ***")
+//    var i = 0
+//    var (_, name, price) = menuList[0].replace("\t", "").split(",")
+//    var longestString = (name + price).count() + 10 //считает длину строки без типа + добавлет 10 точек
+//    while (i <menuList.count()) {
+//        var (_, name1, price1) = menuList[i].replace("\t", "").split(",")
+//        var temp = (name1 + price1).count() + 10
+//        if ( temp > longestString) longestString = temp
+//        i++
+//    }
+//
+//    i = 0
+//    while (i < menuList.count()) {
+//        var (_, name1, price1) = menuList[i].replace("\t", "").split(",")
+//        var temp = (name1 + price1).count() + 10
+//        var dotsCount = longestString - temp + 10
+//        print(name1)
+//        var j = 0
+//        while (j < dotsCount) {
+//            print(".")
+//            j++
+//        }
+//        j = 0
+//        print(price1)
+//        println("")
+//        i++
+//    }
+//}
+
+private fun displayPatronBalances() {
+    patronGolg.forEach { patron, balance ->
+        println("$patron, balance: ${"%.2f".format(balance)}")
     }
 }
 
-fun performPurchase(price: Double) {
-    displayBalance()
-    val totalPurse = playerGold + (playerSilver / 100.0)
-    println("Total purse: $totalPurse")
-    println("Purchasing item for $price")
-
-    val remainingBalance = totalPurse - price
-    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
-
-    val remainingGold = remainingBalance.toInt()
-    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
-    playerGold = remainingGold
-    playerSilver = remainingSilver
-    displayBalance()
-}
-
-private fun displayBalance() {
-    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
+fun performPurchase(price: Double, patronName: String) {
+    val totalPurse = patronGolg.getValue(patronName)
+    patronGolg[patronName] = totalPurse - price
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -73,7 +106,7 @@ private fun placeOrder(patronName: String, menuData: String) {
     val message = "$patronName buys a $name ($type) for $price."
     println(message)
 
-//    performPurchase(price.toDouble())
+    performPurchase(price.toDouble(), patronName)
 
     val phrase = if (name == "Dragon's Breath") {
         "$patronName exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
